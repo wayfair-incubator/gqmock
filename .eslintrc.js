123a2ffc94ff959b7ed05d502a3b3652e056d19c
Mark Faga <mfaga@wayfair.com>
2022-10-06 15:21:21 -0400

    docs: configure jsdocs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,29 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsdoc/recommended',
+    // Make sure this is always the last configuration in the extends array.
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    'jsdoc/require-jsdoc': 'error',
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*'],
+      rules: {
+        'jsdoc/require-jsdoc': 'off',
+      },
     },
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        // Make sure this is always the last configuration in the extends array.
-        'plugin:prettier/recommended',
-    ],
-    rules: {
-        '@typescript-eslint/no-var-requires': 'off',
-        '@typescript-eslint/ban-ts-comment': 'off',
-        '@typescript-eslint/no-unused-vars': 'error',
-        '@typescript-eslint/explicit-module-boundary-types': 'error',
-        '@typescript-eslint/no-non-null-assertion': 'error',
-    },
+  ],
 };
